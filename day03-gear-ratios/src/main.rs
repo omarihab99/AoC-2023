53780a8be1459d39fd671a7b1d06557037499cf0
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-12-09 23:29:30 +0200

    Refactored day03 main file to separate part_one solution into its own function.

--- day03-gear-ratios/src/main.rs
+++ day03-gear-ratios/src/main.rs
@@ -11,54 +11,71 @@ fn main() {
         let mut number: u32 = 0;
         let mut valid = false;
         for (j, c) in line.chars().enumerate() {
-            #[warn(unused_comparisons)]
-            if c.is_digit(10){
-                if i >= 1 {
-                    let top = input[i - 1].chars().nth(j);
-                    valid |= top.is_some() && !top.unwrap().is_digit(10) && top.unwrap() != '.';
-                }
-                if j >= 1 {
-                    let left= input[i].chars().nth(j - 1);
-                    valid |= left.is_some() && !left.unwrap().is_digit(10) && left.unwrap() != '.';
-                }
-                if i+1 < input.len() {
-                    let bottom= input[i + 1].chars().nth(j);
-                    valid |= bottom.is_some() && !bottom.unwrap().is_digit(10) && bottom.unwrap() != '.';
-                }
-                if j+1 < line.len() {
-                    let right= input[i].chars().nth(j + 1);
-                    valid |= right.is_some() && !right.unwrap().is_digit(10) && right.unwrap() != '.';
-                }
-                if i >= 1 && j >= 1 {
-                    let left_up= input[i - 1].chars().nth(j - 1);
-                    valid |= left_up.is_some() && !left_up.unwrap().is_digit(10) && left_up.unwrap() != '.';
-                }
-                if i >= 1 && j+1 < line.len() {
-                    let left_down= input[i - 1].chars().nth(j + 1);
-                    valid |= left_down.is_some() && !left_down.unwrap().is_digit(10) && left_down.unwrap() != '.';
-                }
-                if i+1 < input.len() && j >= 1 {
-                    let right_up= input[i + 1].chars().nth(j - 1);
-                    valid |= right_up.is_some() && !right_up.unwrap().is_digit(10) && right_up.unwrap() != '.';
-                }
-                if i+1 < input.len() && j+1 < line.len() {
-                    let right_down= input[i + 1].chars().nth(j + 1);
-                    valid |= right_down.is_some() && !right_down.unwrap().is_digit(10) && right_down.unwrap() != '.';
-                }
-                number = number * 10 + c.to_digit(10).unwrap();
-            }
-            else {
-                if valid {
-                    sum += number;
-                }
-                valid = false;
-                number = 0;
-            }
+            part_one(&mut valid, &mut number, &input, &c, i, j, &mut sum, &line);
         }
+        // This check is required to solve part one of the problem.
         if valid {
             sum += number;
         }
     }
     println!("{}", sum);
 }
-
+fn part_one(
+    valid: &mut bool,
+    number: &mut u32,
+    input: &Vec<String>,
+    c: &char,
+    i: usize,
+    j: usize,
+    sum: &mut u32,
+    line: &String,
+) {
+    #[warn(unused_comparisons)]
+    if c.is_digit(10) {
+        if i >= 1 {
+            let top = input[i - 1].chars().nth(j);
+            *valid |= top.is_some() && !top.unwrap().is_digit(10) && top.unwrap() != '.';
+        }
+        if j >= 1 {
+            let left = input[i].chars().nth(j - 1);
+            *valid |= left.is_some() && !left.unwrap().is_digit(10) && left.unwrap() != '.';
+        }
+        if i + 1 < input.len() {
+            let bottom = input[i + 1].chars().nth(j);
+            *valid |= bottom.is_some() && !bottom.unwrap().is_digit(10) && bottom.unwrap() != '.';
+        }
+        if j + 1 < line.len() {
+            let right = input[i].chars().nth(j + 1);
+            *valid |= right.is_some() && !right.unwrap().is_digit(10) && right.unwrap() != '.';
+        }
+        if i >= 1 && j >= 1 {
+            let left_up = input[i - 1].chars().nth(j - 1);
+            *valid |=
+                left_up.is_some() && !left_up.unwrap().is_digit(10) && left_up.unwrap() != '.';
+        }
+        if i >= 1 && j + 1 < line.len() {
+            let left_down = input[i - 1].chars().nth(j + 1);
+            *valid |= left_down.is_some()
+                && !left_down.unwrap().is_digit(10)
+                && left_down.unwrap() != '.';
+        }
+        if i + 1 < input.len() && j >= 1 {
+            let right_up = input[i + 1].chars().nth(j - 1);
+            *valid |=
+                right_up.is_some() && !right_up.unwrap().is_digit(10) && right_up.unwrap() != '.';
+        }
+        if i + 1 < input.len() && j + 1 < line.len() {
+            let right_down = input[i + 1].chars().nth(j + 1);
+            *valid |= right_down.is_some()
+                && !right_down.unwrap().is_digit(10)
+                && right_down.unwrap() != '.';
+        }
+        *number = *number * 10 + c.to_digit(10).unwrap();
+    } else {
+        if *valid {
+            *sum += *number;
+        }
+        *valid = false;
+        *number = 0;
+    }
+}
