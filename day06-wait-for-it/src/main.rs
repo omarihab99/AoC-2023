6c527a14991ae5b2e397d21b554f07d4c480db93
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-12-10 20:54:49 +0200

    Solved part two of day06 challenge.

--- day06-wait-for-it/src/main.rs
+++ day06-wait-for-it/src/main.rs
@@ -1,20 +1,26 @@
 // Time:        57     72     69     92
 // Distance:   291   1172   1176   2026
 fn main() {
-    let time : Vec<u32> = vec![57, 72, 69, 92];
-    let mut res : u32 = 1;
-    let distance : Vec<u32> = vec![291, 1172, 1176, 2026];
-    for i in 0..time.len(){
+    let time: Vec<u32> = vec![57, 72, 69, 92];
+    let mut res: u32 = 1;
+    let distance: Vec<u32> = vec![291, 1172, 1176, 2026];
+    for i in 0..time.len() {
         let t = time[i];
         let d = distance[i];
-        let mut sum = 0;
-        for b in 0..t+1{
-            let curr_d = b * (t-b);
-            if curr_d > d{
-                sum += 1;
-            }
-        }
-        res *= sum;
+        let sum = calculate(t as u128, d as u128);
+        res *= sum as u32;
     }
+    // Part one
     println!("{}", res);
+    println!("{}", calculate(57726992, 291117211762026));
+}
+fn calculate(time: u128, distance: u128) -> u128 {
+    let mut sum: u128 = 0;
+    for b in 0..time + 1 {
+        let curr_d: u128 = b * (time - b);
+        if curr_d > distance {
+            sum += 1;
+        }
+    }
+    sum
 }
