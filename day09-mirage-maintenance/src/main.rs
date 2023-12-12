bda07a99275c2f270f56fbaca151c99ef038ae31
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-12-12 23:49:07 +0200

    Solved part two of day09 challenge

--- day09-mirage-maintenance/src/main.rs
+++ day09-mirage-maintenance/src/main.rs
@@ -2,33 +2,46 @@ mod helper;
 
 fn main() {
     let input = helper::read_input();
-    let mut sum : i64 = 0;
+    let mut sum: i64 = 0;
+    let part_two = true;
     for line in input {
-        let mut res : Vec<Vec<i64>> = Vec::new();
+        let mut res: Vec<Vec<i64>> = Vec::new();
         res.push(line.clone());
         get_diff(&line, &mut res);
-        sum+=get_number(&res);
+        sum += if part_two {
+            get_number_two(&res)
+        } else {
+            get_number_one(&res)
+        };
+        println!("{}", sum);
     }
-    println!("Part one solution: {}", sum);
+    println!("solution: {}", sum);
 }
-fn get_diff(line: &Vec<i64>, res: &mut Vec<Vec<i64>>)  {
-    res.push(line.iter().zip(line.iter().skip(1)).map(|(x, y)| y - x).collect::<Vec<i64>>());
+fn get_diff(line: &Vec<i64>, res: &mut Vec<Vec<i64>>) {
+    res.push(
+        line.iter()
+            .zip(line.iter().skip(1))
+            .map(|(x, y)| y - x)
+            .collect::<Vec<i64>>(),
+    );
     let mut not_zero = false;
-    for num in res.last().unwrap(){
-        if num != &0{
+    for num in res.last().unwrap() {
+        if num != &0 {
             not_zero = true;
             break;
         }
     }
     if not_zero {
         let r = res.clone();
-        get_diff(r.last().unwrap(),  res);
+        get_diff(r.last().unwrap(), res);
     }
 }
 
-fn get_number(res: &Vec<Vec<i64>>) -> i64 {
+fn get_number_one(res: &Vec<Vec<i64>>) -> i64 {
     let c = 0;
-    res.iter().fold(c, |acc, x| {
-        acc + x.last().unwrap()
-    })
+    res.iter().rev().fold(c, |acc, x| acc + x.last().unwrap())
+}
+fn get_number_two(res: &Vec<Vec<i64>>) -> i64 {
+    let c = 0;
+    res.iter().rev().fold(c, |acc, x| x.first().unwrap() - acc)
 }
