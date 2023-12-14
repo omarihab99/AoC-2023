33d7f3d841cce3f1691102b5c3bd4aa18ff8da76
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-12-14 23:30:04 +0200

    Increased efficiency by removing redundant space and solved part two.

--- day11-cosmic-expansion/src/helper.rs
+++ day11-cosmic-expansion/src/helper.rs
@@ -1,41 +1,22 @@
-use std::{fs::File, io::{BufRead, BufReader}};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
 pub(crate) fn read_input() -> Vec<Vec<char>> {
     let file = File::open("src/input.txt").unwrap();
     let reader = BufReader::new(file);
-    let mut res : Vec<Vec<char>> = Vec::new();
-    for line in reader.lines(){
-        let l = line.unwrap();
-        res.push(l.chars().collect());
-        if !l.contains('#') {
-            res.push(l.chars().collect());
-        }
-    }
-    let mut col: usize = 0;
-    while col < res[0].len(){
-        let mut found = false;
-        for row in 0..res.len(){
-            if res[row][col] == '#' {
-                found = true;
-                break;
-            }
-        }
-        if !found {
-            res.iter_mut().for_each(| v | {
-                v.insert(col, '.');
-            });
-            col+=1;
-        }
-        col+=1
-    }
-    res
+    reader
+        .lines()
+        .map(|l| l.unwrap().chars().collect())
+        .collect()
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     #[test]
-    fn test_parse_input(){
+    fn test_parse_input() {
         let res = read_input();
-        for (i,row) in res.iter().enumerate() {
+        for (i, row) in res.iter().enumerate() {
             println!("Row #{}: {:?}", i, row);
         }
     }
--- day11-cosmic-expansion/src/main.rs
+++ day11-cosmic-expansion/src/main.rs
@@ -1,9 +1,12 @@
+use std::cmp::{max,min};
+
 mod helper;
 
 fn main() {
     let input = helper::read_input();
-    let mut indices : Vec<(usize, usize)> = Vec::new();
-    let mut sum  = 0;
+    let mut indices: Vec<(usize, usize)> = Vec::new();
+    let mut sum = 0;
+    let part_two = true; // change to false to get part 1
     input.iter().enumerate().for_each(|(i, v)| {
         v.iter().enumerate().for_each(|(j, c)| {
             if *c == '#' {
@@ -11,11 +14,35 @@ fn main() {
             }
         })
     });
-    for (i, v )in indices.iter().enumerate() {
-        for j in i..indices.len() {
+    let (expanded_rows_indices, expanded_cols_indices) = expand(&input);
+    println!("Rows: {:?}", expanded_rows_indices);
+    println!("Cols: {:?}", expanded_cols_indices);
+    for (i, v) in indices.iter().enumerate() {
+        for j in i+1..indices.len() {
             let (x, y) = indices[j];
-            sum += (x as i32 - v.0 as i32).abs() + (y as i32 - v.1 as i32).abs() ;
+            sum += (x as i64 - v.0 as i64).abs()
+                + (y as i64 - v.1 as i64).abs()
+                + expanded_rows_indices.iter().filter(|k| min(v.0,x) <= **k && **k <= max(v.0,x)).count() as i64 * if part_two { 999999 } else { 1 }
+                + expanded_cols_indices.iter().filter(|k| min(v.1,y) <= **k && **k <= max(v.1,y)).count() as i64 * if part_two { 999999 } else { 1 };
         }
     }
     println!("Part one: {}", sum);
+}
+// No need to add new rows or columns to the input due to the expansion and this will be obvious when solving part two in which the expansion is 1 million times larger.
+// Alternatively, we could just add the indices of new rows and columns to count them in part two.
+fn expand(input: &Vec<Vec<char>>) -> (Vec<usize>, Vec<usize>) {
+    let mut expanded_rows_indices: Vec<usize> = Vec::new();
+    let mut expanded_cols_indices: Vec<usize> = Vec::new();
+    input.iter().enumerate().for_each(|(i, v)| {
+        if !v.contains(&'#') {
+            expanded_rows_indices.push(i);
+        }
+    });
+    for i in 0..input[0].len() {
+        let found = input.iter().any(|row| row[i] == '#');
+        if !found {
+            expanded_cols_indices.push(i);
+        }
+    }
+    (expanded_rows_indices, expanded_cols_indices)
 }
