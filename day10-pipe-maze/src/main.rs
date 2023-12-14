b72a08a4187c51381a8aeb3524d4daa08984b9a0
omarihab99 <omar.ihab.mohamed99@gmail.com>
2023-12-14 19:33:37 +0200

    Solved part two of day10 challenge.

--- day10-pipe-maze/src/main.rs
+++ day10-pipe-maze/src/main.rs
@@ -1,32 +1,60 @@
-use std::{collections::{BTreeMap, btree_map::Entry}};
+use std::collections::{btree_map::Entry, BTreeMap, BTreeSet};
 mod helper;
 
 fn main() {
     let (input, start) = helper::read_input();
-    let mut stack  = vec![(start.0, start.1)];
+    println!("{}", part_one(&input, start));
+    println!("{}", part_two(&input, start));
+}
+fn part_two(input: &Vec<Vec<char>>, start: (usize, usize)) -> i32 {
+    let mut stack = vec![(start.0, start.1)];
+    let mut visited: BTreeSet<(usize, usize)> = BTreeSet::new();
+    let mut polygon_loop = vec![(start.0 as i32, start.1 as i32)];
+    visited.insert((start.0, start.1));
+    while let Some((row, col)) = stack.pop() {
+        let current_char = input[row][col];
+        for (adj, row_d, col_d) in get_neighbour_nodes(row, col, &input) {
+            match (adj, current_char, row_d, col_d) {
+                ('|', '7' | 'F' | '|' | 'S', 1, 0)
+                | ('|', 'L' | 'J' | '|' | 'S', -1, 0)
+                | ('-', 'L' | 'F' | '-' | 'S', 0, 1)
+                | ('-', '7' | 'J' | '-' | 'S', 0, -1)
+                | ('L', '7' | 'F' | '|' | 'S', 1, 0)
+                | ('L', '7' | 'J' | '-' | 'S', 0, -1)
+                | ('J', '7' | 'F' | '|' | 'S', 1, 0)
+                | ('J', 'L' | 'F' | '-' | 'S', 0, 1)
+                | ('7', 'L' | 'J' | '|' | 'S', -1, 0)
+                | ('7', 'L' | 'F' | '-' | 'S', 0, 1)
+                | ('F', 'L' | 'J' | '|' | 'S', -1, 0)
+                | ('F', '7' | 'J' | '-' | 'S', 0, -1) => {
+                    let next_row = row as i32 + row_d;
+                    let next_col = col as i32 + col_d;
+                    if visited.insert((next_row as usize, next_col as usize)) {
+                        stack.push((next_row as usize, next_col as usize));
+                        polygon_loop.push((next_row, next_col));
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+    let l = polygon_loop.len() as i32;
+    let mut area = 0;
+    polygon_loop.push((start.0 as i32, start.1 as i32));
+    for ((x1, y1), (x2, y2)) in polygon_loop.iter().zip(polygon_loop.iter().skip(1)) {
+        area += x1 * y2 - x2 * y1;
+    }
+    area.abs() / 2 - (l / 2 - 1)
+}
+fn part_one(input: &Vec<Vec<char>>, start: (usize, usize)) -> u32 {
+    let mut stack = vec![(start.0, start.1)];
     let mut nodes: BTreeMap<(usize, usize), u32> = BTreeMap::new();
     nodes.insert((start.0, start.1), 0);
     while let Some((row, col)) = stack.pop() {
         let current_char = input[row][col];
         let current_dis = nodes[&(row, col)];
-        let m = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-            .into_iter()
-            .filter( |(row_diff, col_diff )| {
-                row as i32 + row_diff >= 0
-                    && row as i32 + row_diff < input.len() as i32
-                    && col as i32 + col_diff >= 0
-                    && col as i32 + col_diff < input[0].len() as i32
-            })
-            .map( |(row_diff, col_diff)| {
-                let next_row =( row as i32 + row_diff) as usize;
-                let next_col = (col as i32 + col_diff) as usize;
-                let adj_char = input[next_row][next_col];
-                (adj_char, row_diff, col_diff)
-            });
-
         // The rules of connectivity imply that some characters can be adjacent to some other characters at specific positions (for example, in '7L', '7' can not be visited from 'L').
-
-        for (adj, row_d, col_d) in m {
+        for (adj, row_d, col_d) in get_neighbour_nodes(row, col, &input) {
             match (adj, current_char, row_d, col_d) {
                 ('|', '7' | 'F' | '|' | 'S', 1, 0)
                 | ('|', 'L' | 'J' | '|' | 'S', -1, 0)
@@ -55,11 +83,31 @@ fn main() {
                             }
                         }
                     }
-                },
+                }
                 _ => (),
             }
         }
     }
-    println!("{}", nodes.values().max().unwrap());
+    *nodes.values().max().unwrap()
 }
 
+fn get_neighbour_nodes(
+    row: usize,
+    col: usize,
+    input: &Vec<Vec<char>>,
+) -> impl Iterator<Item = (char, i32, i32)> + '_ {
+    [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        .into_iter()
+        .filter(move |(row_diff, col_diff)| {
+            row as i32 + row_diff >= 0
+                && row as i32 + row_diff < input.len() as i32
+                && col as i32 + col_diff >= 0
+                && col as i32 + col_diff < input[0].len() as i32
+        })
+        .map(move |(row_diff, col_diff)| {
+            let next_row = (row as i32 + row_diff) as usize;
+            let next_col = (col as i32 + col_diff) as usize;
+            let adj_char = input[next_row][next_col];
+            (adj_char, row_diff, col_diff)
+        })
+}
